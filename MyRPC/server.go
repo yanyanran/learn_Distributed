67925e6e546cb8438b66d14c4af57f13e46f836b
yanyanran <yanran66666@qq.com>
2023-01-17 21:21:01 +0800

    集成到服务端

--- MyRPC/server.go
+++ MyRPC/server.go
@@ -2,18 +2,22 @@ package MyRPC
 
 import (
 	"encoding/json"
-	"fmt"
+	"errors"
 	"io"
 	"log"
 	"myrpc/codec"
 	"net"
 	"reflect"
+	"strings"
 	"sync"
 )
 
 const MagicNum = 0x3bef5c
 
-type Server struct{}            // an rpc server
+type Server struct { // an rpc server
+	serviceMap sync.Map // 并发安全的 map
+}
+
 var DefaultServer = NewServer() // *Server的默认实例
 
 // Option 客户端可选择不同的编解码器来编码消息body
@@ -73,6 +77,42 @@ func (server *Server) ServerConn(conn io.ReadWriteCloser) {
 	server.serverCodec(f(conn))
 }
 
+// Register 在server中发布一组方法
+func (server *Server) Register(rcvr interface{}) error {
+	s := newService(rcvr)
+	if _, dup := server.serviceMap.LoadOrStore(s.name, s); dup {
+		return errors.New("rpc:服务已定义：" + s.name)
+	}
+	return nil
+}
+
+// Register 在默认Server中发布接收方的方法
+func Register(rcvr interface{}) error {
+	return DefaultServer.Register(rcvr)
+}
+
+// findService 通过ServiceMethod从serviceMap中找到对应的service
+func (server *Server) findService(serviceMethod string) (svc *service, mtype *methodType, err error) {
+	dot := strings.LastIndex(serviceMethod, ".")
+	if dot < 0 {
+		err = errors.New("rpc服务器：service/method请求格式错误：" + serviceMethod)
+		return
+	}
+	serviceName, methodName := serviceMethod[:dot], serviceMethod[dot+1:] // serviceMethod=>Service.Method分成两部分（service名+方法名）
+	// 先在【serviceMap】中找到对应的service实例，再从【service实例的method】中找到对应的methodType
+	svci, ok := server.serviceMap.Load(serviceName) // 读key得value
+	if !ok {
+		err = errors.New("rpc服务器：找不到service" + serviceName)
+		return
+	}
+	svc = svci.(*service)
+	mtype = svc.method[methodName]
+	if mtype == nil {
+		err = errors.New("rpc服务器：找不到method" + methodName)
+	}
+	return
+}
+
 var invalidRequest = struct{}{} // 发生错误时响应argv的占位符
 
 // serverCodec 对请求读、回复、处理
@@ -107,6 +147,8 @@ type request struct {
 	h      *codec.Header
 	argv   reflect.Value // 反射值对象（.interface()获取反射实例）
 	replyv reflect.Value
+	mtype  *methodType
+	svc    *service
 }
 
 func (server *Server) readRequestHeader(cc codec.Codec) (*codec.Header, error) {
@@ -128,9 +170,20 @@ func (server *Server) readRequest(cc codec.Codec) (*request, error) {
 	}
 	req := &request{h: h}
 	// TODO: 现在我们不确定请求argv的类型，先假设是字符串
-	req.argv = reflect.New(reflect.TypeOf(""))
-	if err = cc.ReadBody(req.argv.Interface()); err != nil { // 如果readBody出错了 还能返回错误给客户端（客户端还存在
-		log.Println("rpc服务器：读取argv错误：", err)
+	req.argv = req.mtype.newArgv() // 创建两个入参实例
+	req.replyv = req.mtype.newReplyv()
+
+	argvi := req.argv.Interface()
+	if req.argv.Type().Kind() != reflect.Ptr {
+		argvi = req.argv.Addr().Interface() // 确保argvi是指针->(ReadBody需要一个指针作参
+	}
+	/*	req.argv = reflect.New(reflect.TypeOf(""))
+		if err = cc.ReadBody(req.argv.Interface()); err != nil { // 如果readBody出错了 还能返回错误给客户端（客户端还存在
+			log.Println("rpc服务器：读取argv错误：", err)
+		}*/
+	if err = cc.ReadBody(argvi); err != nil { // cc.ReadBody将请求报文反序列化为第一个入参argv
+		log.Println("rpc服务器：读取body错误：", err)
+		return req, err
 	}
 	return req, nil
 }
@@ -147,9 +200,15 @@ func (server *Server) sendResponse(cc codec.Codec, h *codec.Header, body interfa
 // handleRequest 处理请求
 func (server *Server) handleRequest(cc codec.Codec, req *request, send *sync.Mutex, wait *sync.WaitGroup) {
 	// TODO 应调用已注册的rpc方法以获得正确的replyv
-	// 简单的，目前--只需打印argv并发送hello消息
 	defer wait.Done()
-	log.Println(req.h, req.argv.Elem())                               // reflect.Elem()通过反射获取指针指向的元素类型
-	req.replyv = reflect.ValueOf(fmt.Sprintf("myrpc响应%d", req.h.Seq)) /// Seq客户端请求序列号
-	server.sendResponse(cc, req.h, req.replyv.Interface(), send)
+	/*	// 简单的，目前--只需打印argv并发送hello消息
+		log.Println(req.h, req.argv.Elem())                                 // reflect.Elem()通过反射获取指针指向的元素类型
+		req.replyv = reflect.ValueOf(fmt.Sprintf("myrpc响应%d", req.h.Seq)) /// Seq客户端请求序列号*/
+	err := req.svc.call(req.mtype, req.argv, req.replyv) // call完成方法调用
+	if err != nil {
+		req.h.Error = err.Error()
+		server.sendResponse(cc, req.h, invalidRequest, send)
+		return
+	}
+	server.sendResponse(cc, req.h, req.replyv.Interface(), send) // 将replyv传给sendResponse完成序列化
 }
