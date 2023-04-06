a328805f9749dd0c33beffcce1099f56ae47dd51
yanyanran <yanran66666@qq.com>
2023-04-06 11:53:34 +0800

    topic完善

--- MicroService/MQ/message/topic.go
+++ MicroService/MQ/message/topic.go
@@ -16,6 +16,7 @@ type Topic struct {
 	readSyncChan        chan struct{}
 	routerSyncChan      chan struct{}
 	exitChan            chan util.ChanReq // 是否已向channel发送消息
+	channelWriteStarted bool              //是否已经向channel发送消息
 }
 
 var ( // 全局
@@ -76,18 +77,82 @@ func (t *Topic) GetChannel(channelName string) *Channel {
 	return (<-channelRet).(*Channel)
 }
 
-func (t *Topic) Router(inMemSize int) {
+func (t *Topic) PutMessage(msg *Message) {
+	t.incomingMessageChan <- msg
+}
+
+func (t *Topic) MessagePump(closeChan <-chan struct{}) {
+	var msg *Message
 	for {
 		select {
-		case channelReq := <-t.newChannelChan:
+		case msg = <-t.msgChan:
+		case <-closeChan:
+			return
+		}
+
+		t.readSyncChan <- struct{}{} // 读管道阻塞-> 加锁
+
+		for _, channel := range t.channelMap {
+			go func(ch *Channel) {
+				ch.PutMessage(msg) // 消息推送给channel考虑原子问题(map并发读写错误)
+			}(channel)
+		}
+
+		t.routerSyncChan <- struct{}{} // 数据到来-> 解锁
+	}
+}
+
+func (t *Topic) Close() error {
+	errChan := make(chan interface{})
+	t.exitChan <- util.ChanReq{
+		RetChan: errChan,
+	}
+	err, _ := (<-errChan).(error)
+	return err
+}
+
+func (t *Topic) Router(inMemSize int) {
+	var (
+		msg       *Message
+		closeChan = make(chan struct{})
+	)
+	for {
+		select {
+		case channelReq := <-t.newChannelChan: // 添加channel topic开始推送协程给channel
 			channelName := channelReq.Variable.(string)
 			channel, ok := t.channelMap[channelName]
 			if !ok {
 				channel = NewChannel(channelName, inMemSize)
 				t.channelMap[channelName] = channel
 				log.Printf("TOPIC(%s): new channel(%s)", t.name, channel.name)
+				channelReq.RetChan <- channel
+				if !t.channelWriteStarted {
+					go t.MessagePump(closeChan) // topic消息推送到channel协程
+					t.channelWriteStarted = true
+				}
 			}
-			channelReq.RetChan <- channel
+
+		case msg = <-t.incomingMessageChan:
+			select {
+			case t.msgChan <- msg:
+				log.Printf("TOPIC(%s) wrote message", t.name)
+			default: // msgChan满了丢弃
+				// TODO：持久化磁盘功能
+			}
+		case <-t.readSyncChan:
+			<-t.routerSyncChan
+		case closeReq := <-t.exitChan:
+			log.Printf("TOPIC(%s): closing", t.name)
+
+			for _, channel := range t.channelMap {
+				err := channel.Close()
+				if err != nil {
+					log.Printf("ERROR: channel(%s) close - %s", channel.name, err.Error())
+				}
+			}
+
+			close(closeChan)
+			closeReq.RetChan <- nil
 		}
 	}
 }
